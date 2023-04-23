c8d7c82e5172aa98998cb022e23fd74b680726a8
BrookeMorgann <102631061+BrookeMorgann@users.noreply.github.com>
2023-04-22 23:29:55 -0400

    trying to add in my chart

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-
-
-
 function injectHTML(list) {
-    console.log('yo mama')
-    const target = document.querySelector('.people_list');
-    target.innerHTML = '';
-    // const arr = Array.from(list);
-    list.forEach(item => {
-        const str = `<li>${item.uid}</li>`;
-        // const str = `<table>${item.description}</table>`;
-        target.innerHTML += str})
-    console.log('fired injectHTML')
-    
+  console.log("injected HTML");
+  const target = document.querySelector(".people_list");
+  target.innerHTML = "";
+  list.forEach((item) => {
+    const str = `<table>${item.sex}</table>`;
+    target.innerHTML += str;
+  });
+}
+
+
+async function mainEvent() {
+  const mainForm = document.querySelector(".main_form");
+  const loadDataButton = document.querySelector("#data_load");
+  const generateListButton = document.querySelector("#generate");
+
+  let currentList = []; // this is "scoped" to the main event function
+
+  loadDataButton.addEventListener("click", async (submitEvent) => {
+    // async has to be declared on every function that needs to "await" something
+
+    console.log("load data");
+
+    let results = await fetch("https://api.fbi.gov/wanted/v1/list");
+    currentList = await results.json();
+
+    storedData = JSON.stringify(currentList);
+    parsedData = JSON.parse(storedData);
+
+    console.log("PD", parsedData);
+  });
+
+  generateListButton.addEventListener("click", (event) => {
+    console.log("generate list");
+    data = parsedData.items;
+    console.log(data);
+    injectHTML(data);
+  });
+
+  
 
 }
-  
-async function mainEvent() { 
-    const mainForm = document.querySelector('.main_form');
-    const loadDataButton = document.querySelector('#data_load');
-    const generateListButton = document.querySelector('#generate');
-  
-    let currentList = []; // this is "scoped" to the main event function
-    
-    loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-      
-        console.log('load data'); 
-
-        let results = await fetch('https://api.fbi.gov/wanted/v1/list');
-  
-      // This changes the response from the GET into data we can use - an "object"
-        currentList = await results.json();
-
-        storedData = JSON.stringify(currentList);
-        parsedData = JSON.parse(storedData);
-
-        console.log('PD', parsedData)
-      
-    });
-
-    generateListButton.addEventListener('click', (event) => {
-        console.log('generate list');
-        data = parsedData.items[1];
-        console.log('br', data);
-        injectHTML(data);
-    })
-
-}
-document.addEventListener('DOMContentLoaded', async () => mainEvent());
+document.addEventListener("DOMContentLoaded", async () => mainEvent());
